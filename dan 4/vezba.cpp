1935b6f42312e7918645ad77abcc2edbe1195b73
Uros Markovic <Uros.Markovic@Syrmia.com>
2024-04-11 11:12:36 +0200

    Zavrsen i treci deo zadatka iz Dan4.

--- dan 4/vezba.cpp
+++ dan 4/vezba.cpp
@@ -1,5 +1,8 @@
 #include <iostream>
 #include <type_traits>
+#include <cstdarg>
+
+#define my_printf(X, ...) _my_printf((X), __VA_ARGS__, NULL)
 
 template <typename T, typename... Args>
 constexpr decltype(auto) sum(T x, Args... args)
@@ -19,11 +22,70 @@ constexpr decltype(auto) min(T x, Args... args)
         return x < min(args...) ? x : min(args...);
 }
 
-// template <const char*,typename... Args>
-// void my_printf(const char * x, Args... args)
-// {
-//     printf(x);
-// }
+void _my_printf(const char *str, ...)
+{
+
+    va_list va;
+    va_start(va, str);
+    std::string sout = "";
+    int i = 0;
+    while (str[i] != '\0')
+    {
+        if (str[i] != '%')
+            sout += str[i++];
+        else
+        {
+            ++i;
+            double f;
+            std::string strf;
+            char *cptr;
+            int d;
+            switch (str[i])
+            {
+            case 's':
+                cptr = va_arg(va, char *);
+                if (cptr == NULL)
+                {
+                    sout = "";
+                    std::cout << "Invalid number of arguments";
+                }
+                else
+                    sout += cptr;
+                break;
+            case 'f':
+                f = va_arg(va, double);
+                if (f == NULL)
+                {
+                    sout = "";
+                    std::cout << "Invalid number of arguments";
+                }
+                else
+                    strf = std::to_string(f);
+                    strf.erase(strf.find_last_not_of('0')+1,std::string::npos);
+                    sout += strf;
+                break;
+            case 'd':
+                d = va_arg(va, int);
+                if (d == NULL)
+                {
+                    sout = "";
+                    std::cout << "Invalid number of arguments";
+                }
+                else
+                    sout += std::to_string(d);
+                break;
+            default:
+                sout = "";
+                std::cout << "Incorrect type / Invalid number of arguments";
+                break;
+            }
+            ++i;
+        }
+    }
+    if (sout != "")
+        std::cout << sout;
+    va_end(va);
+}
 
 int main(int argc, char const *argv[])
 {
@@ -38,7 +100,13 @@ int main(int argc, char const *argv[])
     // min = -58
     std::cout << min(2.2, 1.1, 4, 4, -58) << std::endl;
     // Test #1: OK
-    // my_printf("Simple check\nNew line\nAgain new line\n");
+    my_printf("Simple check\nNew line\nAgain new line\n", 1);
+    // Test #2: Invalid number of arguments
+    my_printf(" text %s, integral value %d than something %d\n", "SUCCESSFULLY", 85);
+    // Test #3: Incorrect type / Invalid number of arguments
+    my_printf("Some text %s, some floating point number %f again something %k\n", "45", 89.7, 56);
+    // Test #4: OK
+    my_printf("Some text %s, some floating point number %f again something %d\n", "45", 89.7, 56);
 
     return 0;
 }
